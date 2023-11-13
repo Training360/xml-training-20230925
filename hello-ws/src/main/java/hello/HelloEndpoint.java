6d53faad4dd6f5a9f59e4d65be60786186a17b8e
Viczián István <viczian.istvan@gmail.com>
2023-11-13 12:46:00 +0100

    Actuator

--- hello-ws/src/main/java/hello/HelloEndpoint.java
+++ hello-ws/src/main/java/hello/HelloEndpoint.java
@@ -5,9 +5,11 @@ import jakarta.jws.WebParam;
 import jakarta.jws.WebResult;
 import jakarta.jws.WebService;
 import jakarta.jws.soap.SOAPBinding;
+import lombok.extern.slf4j.Slf4j;
 
 @WebService(serviceName = "HelloWebService", targetNamespace = HelloEndpoint.HELLO_NS)
 @SOAPBinding(parameterStyle = SOAPBinding.ParameterStyle.BARE)
+@Slf4j
 public class HelloEndpoint {
 
     public static final String HELLO_NS = "https://training360.com/hello";
@@ -15,6 +17,7 @@ public class HelloEndpoint {
     @WebMethod(operationName = "sayHello")
     @WebResult(name = "HelloResponse")
     public HelloResp hello(@WebParam(name = "HelloRequest") HelloRequest request) {
+      log.debug("hello()");
       return new HelloResp("Hello %s!".formatted(request.getName()));
     }
 }
